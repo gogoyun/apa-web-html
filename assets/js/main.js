e0d7804e3df06340e38a0168abd202081c171f6d
gogoyun <gojohnun@gmail.com>
2023-04-24 18:27:02 +0800

    230424 Fix 背景圖替換, Add swiper、download、footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -159,21 +159,33 @@
 	}
 
 	/**
+	 * Testimonials slider
+	 */
+	new Swiper('.testimonials-slider', {
+		speed: 600,
+		loop: true,
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false
+		},
+		slidesPerView: 'auto',
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+
+	/**
 	 * Clients Slider
 	 */
 	new Swiper('.clients-slider', {
 		speed: 400,
 		loop: true,
 		autoplay: {
-			delay: 5000,
+			delay: 3000,
 			disableOnInteraction: false
 		},
 		slidesPerView: 'auto',
-		pagination: {
-			el: '.swiper-pagination',
-			type: 'bullets',
-			clickable: true
-		},
 		breakpoints: {
 			320: {
 				slidesPerView: 2,
@@ -188,81 +200,9 @@
 				spaceBetween: 80
 			},
 			992: {
-				slidesPerView: 6,
+				slidesPerView: 5,
 				spaceBetween: 120
 			}
-		}
-	});
-
-	/**
-	 * Porfolio isotope and filter
-	 */
-	window.addEventListener('load', () => {
-		let portfolioContainer = select('.portfolio-container');
-		if (portfolioContainer) {
-			let portfolioIsotope = new Isotope(portfolioContainer, {
-				itemSelector: '.portfolio-item'
-			});
-
-			let portfolioFilters = select('#portfolio-flters li', true);
-
-			on('click', '#portfolio-flters li', function(e) {
-				e.preventDefault();
-				portfolioFilters.forEach(function(el) {
-					el.classList.remove('filter-active');
-				});
-				this.classList.add('filter-active');
-
-				portfolioIsotope.arrange({
-					filter: this.getAttribute('data-filter')
-				});
-				portfolioIsotope.on('arrangeComplete', function() {
-					AOS.refresh()
-				});
-			}, true);
-		}
-
-	});
-
-	/**
-	 * Initiate portfolio lightbox 
-	 */
-	const portfolioLightbox = GLightbox({
-		selector: '.portfolio-lightbox'
-	});
-
-	/**
-	 * Portfolio details slider
-	 */
-	new Swiper('.portfolio-details-slider', {
-		speed: 400,
-		loop: true,
-		autoplay: {
-			delay: 5000,
-			disableOnInteraction: false
-		},
-		pagination: {
-			el: '.swiper-pagination',
-			type: 'bullets',
-			clickable: true
-		}
-	});
-
-	/**
-	 * Testimonials slider
-	 */
-	new Swiper('.testimonials-slider', {
-		speed: 600,
-		loop: true,
-		autoplay: {
-			delay: 5000,
-			disableOnInteraction: false
-		},
-		slidesPerView: 'auto',
-		pagination: {
-			el: '.swiper-pagination',
-			type: 'bullets',
-			clickable: true
 		}
 	});
 
@@ -277,10 +217,5 @@
 			mirror: false
 		});
 	});
-
-	/**
-	 * Initiate Pure Counter 
-	 */
-	new PureCounter();
 
 })()
